3cfc26911df50f9ab45150ae8f78955b9082e1fa
kabyanpathak <kabyan388@gmail.com>
2024-06-10 19:50:30 -0700

    Update Guard Movement.cs

--- My project/Assets/Kabyan/Guard Movement.cs
+++ My project/Assets/Kabyan/Guard Movement.cs
@@ -13,17 +13,24 @@ public class GuardMovements : MonoBehaviour
     public float speed;
     public Vector3 fin_location;
 
+    private bool loc_reached = true;
+
+
     private void Update()
     {
-        StartCoroutine(Chill());
         if(!player_info.canSeePlayer)
         {
             StartCoroutine(Patrol()); //let him be if not
+            If_Reached();
         }
         else if(player_info.canSeePlayer)
         {
             Chase();
-            
+            If_Reached();
+        }
+        while(loc_reached)
+        {
+            Random_Number();
         }
 
     }
@@ -31,7 +38,6 @@ public class GuardMovements : MonoBehaviour
     //movement when player not spotted
     private IEnumerator Patrol()
     {
-        fin_location = Random_Number();
         transform.position = Vector3.MoveTowards(transform.position, fin_location, speed * Time.deltaTime); //move towards the random value with (speed) velocity
         yield return new WaitForSecondsRealtime(2f);
 
@@ -42,18 +48,23 @@ public class GuardMovements : MonoBehaviour
         transform.position = Vector3.MoveTowards(transform.position, player_info.playerRef.transform.position, speed * Time.deltaTime);
     }
     //gets random vector value to move towards
-    private Vector3 Random_Number()
+    private void Random_Number()
     {
 
         float ran_X = Random.Range(min_X, max_X);
         float ran_Z = Random.Range(min_Z, max_Z);
-        return new Vector3(ran_X,y_Value,ran_Z);
+        fin_location = new Vector3(ran_X,y_Value,ran_Z);
+        loc_reached = false;
 
     }
-    //just waits for whatever value of wait it is(in case code runs slow)
-    private IEnumerator Chill()
+    private void If_Reached()
     {
-        yield return new WaitForSeconds(20f);
+
+        if(transform.position == fin_location)
+        {
+            loc_reached = true;
+        }
+
     }
 
 

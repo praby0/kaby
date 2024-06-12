b2cb6daa74f0533ae2f1ce3207c1f652c1c1e813
praby0 <prabhnoor.singh.4945@gmail.com>
2024-06-11 22:13:54 -0700

    pick up item script(Not finished)

--- My project/Assets/Kabyan/Pick Up Items.cs
+++ My project/Assets/Kabyan/Pick Up Items.cs
@@ -1,18 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using UnityEditor.Callbacks;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class PickUpItems : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public bool equipped;
+    public static bool slotFull;
+
+    public GameObject[] goi;
+    public Vector3 diffFromPlayerToObject;
     void Start()
     {
+        goi = GameObject.FindGameObjectsWithTag("items");
         
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        foreach (GameObject g in goi)
+        {
+            if(g != null)
+            {
+                diffFromPlayerToObject = g.transform.position - transform.position;
+                float currdiff = diffFromPlayerToObject.sqrMagnitude;
+                if(!equipped && currdiff < 2 && Input.GetKeyDown(KeyCode.E) && !slotFull)
+                {
+                    pickUp(g);
+                    print("pick-up");
+                }
+                if(equipped && Input.GetKeyDown(KeyCode.Q))
+                {
+                    drop(g);
+                    print("drop");
+                }
+            }
+        }
+    }
+
+    private void pickUp(GameObject a)
+    {
+        equipped = true;
+        slotFull = true;
+
+    }
+    private void drop(GameObject a)
+    {
+        equipped = false;
+        slotFull = false;
+
     }
 }

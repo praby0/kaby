73f3151762ac3640632a6a6eab1402dd7836145b
praby0 <prabhnoor.singh.4945@gmail.com>
2024-06-13 14:22:05 -0700

    fixed flashlight pickup/drop

--- My project/Assets/C# scripts/Pick Up Items.cs
+++ My project/Assets/C# scripts/Pick Up Items.cs
@@ -84,6 +84,7 @@ public class PickUpItems : MonoBehaviour
     void DropObject()
     {
         //re-enable collision with player
+        equipped = false;
         Physics.IgnoreCollision(heldObj.GetComponent<Collider>(), player.GetComponent<Collider>(), false);
         heldObj.layer = 0; //object assigned back to default layer
         heldObjRb.isKinematic = false;
@@ -93,6 +94,7 @@ public class PickUpItems : MonoBehaviour
     void MoveObject()
     {
         heldObj.transform.position = holdPos.transform.position;
+        heldObj.transform.rotation = holdPos.rotation;
 
     }
     void RotateObject()

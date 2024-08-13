46f9fbda4f9d9dd87317758e70cb536a6b65dbdf
kabyanpathak <kabyan388@gmail.com>
2024-08-12 20:33:46 -0700

    Created Level Selector

--- My project/Assets/Scenes/Main Menu Assets/MainMenu.cs
+++ My project/Assets/Scenes/Main Menu Assets/MainMenu.cs
@@ -10,9 +10,23 @@ public class MainMenu : MonoBehaviour
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
+    public void level1()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void level2()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
+    public void level3()
+    {   
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 3);
+    }
+
     public void back()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex -1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
     }
 
     public void quit()

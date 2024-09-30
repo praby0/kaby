cb539662fada5f444cb3c1f272207792f4070f68
kabyanpathak <kabyan388@gmail.com>
2024-09-30 08:31:39 -0700

    Guard Movements
    I fixed the guard patrol function in test scene.

--- My project/Assets/Scenes/Test Scene Assets/NavMesh Movement Code.cs
+++ My project/Assets/Scenes/Test Scene Assets/NavMesh Movement Code.cs
@@ -12,24 +12,46 @@ public class GuardCode : MonoBehaviour
     Vector3 fin_Location;
     [SerializeField]
     LayerMask groundlayer, playerlayer;
-    public NavMeshAgent agent;
+    NavMeshAgent agent;
+    [SerializeField]
+    float x,y,z;
+    float speed = 5;
 
 
-
-
-    void start()
+    void Start()
     {
-
-
+        agent = GetComponent<NavMeshAgent>();
+        agent.speed = speed;
     }
 
     // Update is called once per frame
     void Update()
     {
         
-        //check if agent has reached final location
-        //if true gets new location
-
+        if(agent.velocity == new Vector3(0,0,0))
+        {
+            fin_Location = NewDestination();
+            agent.SetDestination(fin_Location);
+            Stop();
+        }
+        Stop();
 
     }
+
+    IEnumerator Stop()
+    {
+        yield return 1f;
+    }
+
+    Vector3 NewDestination()
+    {
+
+        x = Random.Range(-10,10);
+        y = 1;
+        z = Random.Range(-10,10);
+
+        return new Vector3(x,y,z);
+
+    }
+
 }
